348000b1df6cce4705b5e306692083632e364a57
Michele Bianchi <jazzinghen@gmail.com>
2011-11-02 10:58:32 +0100

    Added Hardware Shading.

--- Exercises/Week05/JazzBuild.sh
+++ Exercises/Week05/JazzBuild.sh
@@ -7,7 +7,7 @@ for i in `seq -w 6`
 do
   if [ -f "Ex-05-0$i.cpp" ]
   then
-    gcc "Ex-05-0$i.cpp" -lglut -lGLU -lGL -o "bin/Ex-05-0$i"  && echo "\t --Excercise $i"
+    gcc "Ex-05-0$i.cpp"  -lglut -lGLU -lGL -lSOIL -o "bin/Ex-05-0$i"  && echo "\t --Excercise $i"
   else
     echo "\t --Excercise $i still not done. Keep it up!"
   fi
