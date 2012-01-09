2336f430cbcd38ea3f804c5ec9828a09880c8730
Nicolas Silva <nical.silva@gmail.com>
2012-01-09 15:55:35 +0100

    week 11, started part 2

--- Exercises/Week11/BUILD.sh
+++ Exercises/Week11/BUILD.sh
@@ -4,4 +4,5 @@ if [ ! -d "bin" ]; then
 fi
 echo "compiling Week 11 exercises..."
 gcc 02561-11-00-2011.cpp -lglut -lGLU -lGL -o bin/Part1  && echo "--Part1"
+gcc oglpg_example.cpp -lglut -lGLU -lGL -o bin/Part2  && echo "--Part2"
 echo "...done."
