c68f150306853d25214f0134492524ca8ec020b7
Nicolas Silva <nical.silva@gmail.com>
2012-01-09 14:22:10 +0100

    week 11: bezier + nurbs + uniform b spline curves working

--- Exercises/Week11/02561-11-00-2011.cpp
+++ Exercises/Week11/02561-11-00-2011.cpp
@@ -18,6 +18,9 @@ GLfloat ctrlpoints [7][3] = {
 	{10.,5.,0.},
 	{10.,0.,0.}};
 
+// glu NURBS curve
+GLUnurbsObj* nurbsCurve;
+
 
 float factorial(int n)
 {
@@ -48,13 +51,12 @@ vec<3> vecCtrlPoints [7];
 
 // nth order besizer curve (general)
 template<typename T>
-T BezierCurve( T * points, int n, float t )
+T BezierCurve( T * points, int order, float t )
 {
+    int n = order-1;
     T result;
     for(int dim = 0; dim < T::Dimension; ++dim)
     {
-        //std::cout << "-------" << std::endl;
-        //std::cout << pow(t,1) << std::endl;
         for(int i = 0; i <= n; ++i)
         {
             result[dim] += 
@@ -64,24 +66,9 @@ T BezierCurve( T * points, int n, float t )
                 * points[i][dim];
         }
     }
-    //cout << pow(0.5, 0) << endl;
     return result;
 }
 
-// Cubic besizer curve (specific)
-template<typename T>
-T CubicBezierCurve( T* points, float t )
-{
-    T result;
-    for(int dim = 0; dim < T::Dimension; ++dim)
-    {
-        result[dim] = pow(1-t,3) * points[0][dim] 
-            + 3*pow(1-t,2)*t*points[1][dim] 
-            + 3*(1-t)*pow(t,2)*points[2][dim] 
-            + pow(t,3)*points[3][dim];
-    }
-    return result;
-}
 
 void nurbsError(GLenum errorCode)
 {
@@ -91,9 +78,6 @@ void nurbsError(GLenum errorCode)
     exit (0);
 }
 
-GLUnurbsObj* nurbsCurve;
-GLfloat nurbsKnot[] = {1.0,2.0,3.0,4.0,5.0,6.,7.0,8.0,9.0,10.0};
-GLuint nurbsKnotNb = 10;
 
 void init(void)
 {
@@ -105,11 +89,7 @@ void init(void)
         for(int j = 0; j < 3; ++j)
             vecCtrlPoints[i][j] = ctrlpoints[i][j];
     }
-
-    // 4th order bezier curve using legacy OpenGL facilities
-    glMap1f(GL_MAP1_VERTEX_3, 0.0, 1.0, 3, 5, &ctrlpoints[0][0]);
-    glEnable(GL_MAP1_VERTEX_3);
-
+    
     // glu NURBS
     nurbsCurve = gluNewNurbsRenderer();
     assert( nurbsCurve );
@@ -117,6 +97,30 @@ void init(void)
 	gluNurbsProperty(nurbsCurve, GLU_U_STEP, 100);
     gluNurbsCallback(nurbsCurve, GLU_ERROR, (GLvoid (*)()) nurbsError);
     assert( glGetError() == GL_NO_ERROR );
+}
+
+void DisplayNURBSCurve(GLUnurbsObj* curve, GLfloat* knots, GLuint nbKnots )
+{
+    assert( glGetError() == GL_NO_ERROR );
+    gluBeginCurve(nurbsCurve);
+    gluNurbsCurve(
+        curve, nbKnots, knots
+        , 3, &ctrlpoints[0][0]
+        , 4, GL_MAP1_VERTEX_3);
+    gluEndCurve(nurbsCurve);
+    assert( glGetError() == GL_NO_ERROR );
+}
+
+void DisplayGenericBezierCurve( vec<3>* points, int order )
+{
+    // our generic bezier curve implementation
+    glBegin(GL_LINE_STRIP);
+    for(int i = 0; i < 42; ++i)
+    {
+        vec<3> p = BezierCurve(points, order, (float)i/42.0 );
+        glVertex3f(p[0],p[1],p[2]);
+    }
+    glEnd();
 }
 
 void display(void)
@@ -129,7 +133,9 @@ void display(void)
     gluLookAt (0., 5., 1., 0., 5., 0., 0., 1., 0.);
 
 
-    // opengl 4th order bezier curve
+    // opengl 4th order (cubic) bezier curve
+    glMap1f(GL_MAP1_VERTEX_3, 0.0, 1.0, 3, 4, &ctrlpoints[0][0]);
+    glEnable(GL_MAP1_VERTEX_3);
     glColor3f(0.0, 1.0, 0.0);
     glBegin(GL_LINE_STRIP);
       for (i = 0; i <= 30; i++) 
@@ -153,7 +159,7 @@ void display(void)
       glVertex3fv(&ctrlpoints[i][0]);
     }
     glEnd();
-
+/*
     // our generic bezier curve implementation
     glColor3f(1.0, 0.0, 0.0);
     glBegin(GL_LINE_STRIP);
@@ -163,18 +169,47 @@ void display(void)
         glVertex3f(p[0],p[1],p[2]);
     }
     glEnd();
+*/
+    glColor3f(1.0, 0.0, 0.0);
+    DisplayGenericBezierCurve(vecCtrlPoints, 7);
+    DisplayGenericBezierCurve(vecCtrlPoints, 6);
+    DisplayGenericBezierCurve(vecCtrlPoints, 5);
+    DisplayGenericBezierCurve(vecCtrlPoints, 4);
+    DisplayGenericBezierCurve(vecCtrlPoints, 3);
 
-    // glu NURBS curve
-    glGetError();
+/*
+
+    GLfloat nurbsKnot1[] = {  0.0, 0.0, 0.0, 0.0
+                            , 0.1, 0.5, 0.9
+                            , 1.0, 1.0, 1.0, 1.0 };
+    GLfloat nurbsKnot2[] = {  0.0, 0.0, 0.0, 0.0
+                            , 0.25, 0.5, 0.75
+                            , 1.0, 1.0, 1.0, 1.0 };
+    GLfloat nurbsKnot3[] = {  0.0, 0.0, 0.0, 0.0
+                            , 0.4, 0.5, 0.6
+                            , 1.0, 1.0, 1.0, 1.0 };
+    // the following knot vector is equivalent to a cubic bezier curve
+    GLfloat nurbsKnot4[] = {  0.0, 0.0, 0.0, 0.0
+                            , 0.5, 0.5, 0.5
+                            , 1.0, 1.0, 1.0, 1.0 };
+
+    // uniform
+    GLfloat nurbsKnotUniform[] = {  0.0, 0.1, 0.2, 0.3
+                            , 0.4, 0.5, 0.6
+                            , 0.7, 0.8, 0.9, 1.0 };
+
+    // non uniform with different knob vectors
     glColor3f(0.0, 0.0, 1.0);
-    gluBeginCurve(nurbsCurve);
-    gluNurbsCurve(
-        nurbsCurve, nurbsKnotNb, nurbsKnot, 3
-        , &ctrlpoints[0][0], 4, GL_MAP1_VERTEX_3);
-    gluEndCurve(nurbsCurve);
-    assert( glGetError() == GL_NO_ERROR );
+    DisplayNURBSCurve(nurbsCurve, nurbsKnot1, 11 );
+    DisplayNURBSCurve(nurbsCurve, nurbsKnot2, 11 );
+    DisplayNURBSCurve(nurbsCurve, nurbsKnot3, 11 );
+    DisplayNURBSCurve(nurbsCurve, nurbsKnot4, 11 );
 
-
+    // uniform b spline (knots are uniformly growing)
+    glColor3f(0.6, 0.0, 0.6);
+    DisplayNURBSCurve(nurbsCurve, nurbsKnotUniform, 11 );
+*/
+    
     glFlush();
 }
 
