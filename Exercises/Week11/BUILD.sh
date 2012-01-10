b75bcde4289c8fd0ba7f63b3ef70ede6acc33b03
Nicolas Silva <nical.silva@gmail.com>
2012-01-10 13:38:18 +0100

    week 11 report

--- Exercises/Week11/BUILD.sh
+++ Exercises/Week11/BUILD.sh
@@ -2,8 +2,9 @@
 if [ ! -d "bin" ]; then
   mkdir bin
 fi
-echo "compiling Week 11 exercises..."
-gcc 02561-11-00-2011.cpp -lglut -lGLU -lGL -o bin/Part1  && echo "--Part1"
-gcc oglpg_example.cpp -lglut -lGLU -lGL -o bin/Part2  && echo "--Part2"
-gcc nurbs_trim.cpp -lglut -lGLU -lGL -o bin/Part3  && echo "--Part3"
+echo "compiling Week 11 exercises..."cd ../../
+gcc part1.cpp -lglut -lGLU -lGL -o bin/Part1  && echo "--Part1"
+gcc part2_1.cpp -lglut -lGLU -lGL -o bin/Part2_1  && echo "--Part2_1"
+gcc part2_2.cpp -lglut -lGLU -lGL -o bin/Part2_2  && echo "--Part2_2"
+gcc part3.cpp -lglut -lGLU -lGL -o bin/Part3  && echo "--Part3"
 echo "...done."
